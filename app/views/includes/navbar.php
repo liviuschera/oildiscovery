5462adba166b543f0b099a5df798488ab93197c2
Liviu Schera <liviu115@gmail.com>
2018-12-03 18:23:29 +0930

    add sticky header

--- app/views/includes/navbar.php
+++ app/views/includes/navbar.php
@@ -1,67 +1,65 @@
 <header class="header">
-         <div class="header__upper-half">
-            <div class="header__row">
-            <a href="<?php echo URLROOT; ?>">
-               <img
-                  class="header__logo"
-                  src="<?php echo URLROOT; ?>/images/logo-536x84.png"
-                  alt="Logo of Oildiscovery website"
-                  class="header__img"
-               />
-            </a>
-               <nav class="header__nav-social">
-                  <ul>
-                     <li>
-                        <a class="link" href="">
-                           <svg class="icon">
-                              <use
-                                 xlink:href="<?php echo URLROOT; ?>/images/sprite.svg#icon-youtube"
-                              ></use>
-                           </svg>
-                        </a>
-                     </li>
-                     <li>
-                        <a class="link" href="">
-                           <svg class="icon">
-                              <use
-                                 xlink:href="<?php echo URLROOT; ?>/images/sprite.svg#icon-facebook"
-                              ></use>
-                           </svg>
-                        </a>
-                     </li>
-                     <li>
-                        <a class="link" href="">
-                           <svg class="icon">
-                              <use
-                                 xlink:href="<?php echo URLROOT; ?>/images/sprite.svg#icon-envelope"
-                              ></use>
-                           </svg>
-                        </a>
-                     </li>
-                     <li>
-                        <a class="link" href="">
-                           <svg class="icon">
-                              <use
-                                 xlink:href="<?php echo URLROOT; ?>/images/sprite.svg#icon-paw"
-                              ></use>
-                           </svg>
-                        </a>
-                     </li>
-                  </ul>
-               </nav>
-            </div>
+   <div class="header__wrapper u-div-center">
+      <div class="header__upper-half">
+         <div class="header__row">
+            <img
+               class="header__logo"
+               src="images/logo-536x84.png"
+               alt="Logo of Oildiscovery website"
+               class="header__img"
+            />
+            <nav class="header__nav-social">
+               <ul>
+                  <li>
+                     <a class="link" href="">
+                        <svg class="icon">
+                           <use
+                              xlink:href="images/sprite.svg#icon-youtube"
+                           ></use>
+                        </svg>
+                     </a>
+                  </li>
+                  <li>
+                     <a class="link" href="">
+                        <svg class="icon">
+                           <use
+                              xlink:href="images/sprite.svg#icon-facebook"
+                           ></use>
+                        </svg>
+                     </a>
+                  </li>
+                  <li>
+                     <a class="link" href="">
+                        <svg class="icon">
+                           <use
+                              xlink:href="images/sprite.svg#icon-envelope"
+                           ></use>
+                        </svg>
+                     </a>
+                  </li>
+                  <li>
+                     <a class="link" href="">
+                        <svg class="icon">
+                           <use
+                              xlink:href="images/sprite.svg#icon-paw"
+                           ></use>
+                        </svg>
+                     </a>
+                  </li>
+               </ul>
+            </nav>
          </div>
-         <nav class="header__nav-site">
-            <ul>
-               <li><a href="<?php echo URLROOT; ?>">Home</a></li>
-               <li><a href="<?php echo URLROOT; ?>/pages/about">About</a></li>
-               <li><a href="<?php echo URLROOT; ?>/css/styles.css">Join My Team</a></li>
-               <li><a href="">doTERRA Oils as a Business</a></li>
-               <li><a href="">Blog</a></li>
-               <li><a href="">Shop</a></li>
-               <li><a href="">Contact me</a></li>
-               <li><a href="<?php echo URLROOT; ?>/users/login">Login</a></li>
-               <li><a href="<?php echo URLROOT; ?>/users/register">Register</a></li>
-            </ul>
-         </nav>
-      </header>
+      </div>
+      <nav class="header__nav-site">
+         <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Join My Team</a></li>
+            <li><a href="">doTERRA Oils as a Business</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Shop</a></li>
+            <li><a href="">Contact me</a></li>
+         </ul>
+      </nav>
+   </div>
+</header>
