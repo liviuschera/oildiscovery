18f5e8748858ef06329fde2faef10d591f057ea9
Liviu Schera <liviu115@gmail.com>
2019-01-06 16:56:42 +0930

    add post page works

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -22,16 +22,51 @@ class Posts extends Controller
 
     public function add()
     {
-        $data = [
-            'active' => '',
-            'priv' => '',
-            'title' => '',
-            'content' => '',
-            'activeError' => '',
-            'privError' => '',
-            'titleError' => '',
-            'contentError' => ''
-        ];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Sanitize POST array
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            // echo var_dump($_POST['active']);
+            $data = [
+                'active' => !isset($_POST['active']) ? 'n' : 'y',
+                'priv' => trim($_POST['priv']),
+                'title' => trim($_POST['title']),
+                'content' => trim($_POST['content']),
+                'userId' => $_SESSION['user_id'],
+                'activeError' => '',
+                'privError' => '',
+                'titleError' => '',
+                'contentError' => ''
+            ];
+
+            // Validate title
+            if (empty($data['title'])) {
+                $data['titleError'] = 'Please enter title';
+            }
+
+            // Validate content
+            if (empty($data['content'])) {
+                $data['contentError'] = 'Please enter content';
+            }
+
+            if (empty($data['titleError'] && empty($data['contentError']))) {
+                # code...
+            } else {
+                // Load the view with errors
+                $this->view('post/add', $data);
+            }
+        } else {
+            // Display the blank form
+            $data = [
+                'active' => !isset($_POST['active']) ? 'n' : 'y',
+                'priv' => '0',
+                'title' => '',
+                'content' => '',
+                'activeError' => '',
+                'privError' => '',
+                'titleError' => '',
+                'contentError' => ''
+            ];
+        }
 
         $this->view('posts/add', $data);
     }
--- app/views/posts/add.php
+++ app/views/posts/add.php
@@ -5,13 +5,14 @@
 <!-- ~~~ REGISTRATION FORM start ~~~ -->
 
 <form
-   action="<?php echo URLROOT; ?>/users/register"
+   action="<?php echo URLROOT; ?>/post/add"
    class="form u-div-center"
    method="post"
 >
    <h3 class="heading-tertiary">Add Post</h3>
    <p class="paragraph">Please fill out all the fields.</p>
    <pre><?php echo var_dump($data); ?></pre>
+
    
    <!-- Post TITLE -->
       <div class="form__group">
@@ -35,53 +36,70 @@
       <!-- Post ACTIVE -->
       <div class="form__group">
          <div class="form__checkbox">
-            <input
-               type="checkbox"
-               name="active"
-               id="active"
-               <?php echo $data['active'] === 'y' ? 'checked' : ''; ?>
-            />
-            <label for="active"><span></span>Activate Post</label>
+            <input type="checkbox" name="active" id="active" value="y"
+            <?php echo $data['active'] === 'y' ? 'checked' : ''; ?>/>
+            <label for="active"><span></span>Active Post</label>
          </div>
       </div>
          
-   
    <!-- Post PRIV -->
    <div class="form__group">
-      <select name="priv" class="form__select">
-         <option value="0">User</option>
-         <option value="1">Admin</option>
-         <option value="2">Owner</option>
+      <?php $privArray = ['0' => 'User', '1' => 'Admin', '2' => 'Owner'];
+// Set an array contining user privilege levels
+?>
+      <select id="priv" name="priv" class="form__select">
+         <?php foreach ($privArray as $key => $value) {
+             echo "<option value={$key}";
+             if ((string) $data['priv'] === (string) $key) {
+                 echo " selected";
+             }
+             echo ">{$value}</option>";
+         } ?>
+         <!-- <option value="1">Admin</option>
+         <option value="2">Owner</option> -->
       </select>
       <label for="priv">Privilege:</label>
    </div>
    </div>
 
-      
-
    <!-- Post CONTENT -->
-   <textarea
-      name="message"
-      id="textarea"
-      rows="7"
-      class="form__textarea  <?php echo !empty($data['contentError'])
-          ? 'form__invalid'
-          : ''; ?>"
-      value="<?php echo $data['content']; ?>"
-      placeholder="Post Content"
-      name="content"
-      id="content"
-   ></textarea>
+   <div class="form__group">
+      <textarea
+         rows="7"
+         class="form__textarea  <?php echo !empty($data['contentError'])
+             ? 'form__invalid'
+             : ''; ?>"
+         placeholder="Post Content"
+         name="content"
+         id="content"
+      ><?php echo $data['content']; ?></textarea>
+      <span class="form__invalid-feedback"><?php echo $data[
+          'contentError'
+      ]; ?></span>
+   </div>
+
       
+   <div class="form__row">
+      <!-- Back Button -->
+      <button
+         class="button"
+         formaction="<?php echo URLROOT; ?>/posts"
+         type="submit"
+      >
+       &laquo; Back
+      </button>
 
    <!-- Post SUBMIT Button -->
       <button
          class="button"
-         formaction="<?php echo URLROOT; ?>/users/add"
+         formaction="<?php echo URLROOT; ?>/posts/add"
          type="submit"
       >
          Add Post
       </button>
+
+      
+   </div>
 
 </form>
 <!-- ~~~ REGISTRATION FORM end ~~~ -->
